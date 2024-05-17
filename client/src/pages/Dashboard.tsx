53bfcc23e97ef6b80fc9bab3d0e1fcce143917bc
Serhii Neliubov <sergeyneliubov@gmail.com>
2024-05-17 02:11:08 +0200

    functional for kanban input

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {FaPlus} from "react-icons/fa";
 import {useState} from "react";
 import {IoMdCheckmark} from "react-icons/io";
+import toast from "react-hot-toast";
 
 type Task = {
     id: number,
@@ -9,13 +10,25 @@ type Task = {
     status: 'todo' | 'in-progress' | 'done'
 }
 
+type Kanban = {
+    id: number,
+    title: string
+}
+
 export default function Dashboard(){
     const [tasks, setTasks] = useState<Task[]>([]);
-    const [isCreateTaskModalOpen, setIsCreateTaskModalOpen] = useState(false);
+    const [kanbans, setKanbans] = useState<Kanban[]>([])
+
     const [newTaskTitle, setNewTaskTitle] = useState('');
+    const [newKanbanTitle, setNewKanbanTitle] = useState('');
+
+    const [isCreateTaskModalOpen, setIsCreateTaskModalOpen] = useState(false);
+    const [isCreateKanbanModalOpen, setIsCreateKanbanModalOpen] = useState(false);
 
     const createTaskHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if(event.key !== 'Enter') return;
+        if(event.key !== 'Enter') {
+            return;
+        }
 
         setTasks([...tasks, {
             id: Date.now(),
@@ -27,9 +40,28 @@ export default function Dashboard(){
         setIsCreateTaskModalOpen(false);
     }
 
+    const createKanbanHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if(!newKanbanTitle.length) {
+            return toast.error('Title is required!');
+        }
+
+        if(event.key !== 'Enter') {
+            return;
+        }
+
+        const kanban = {
+            id: Date.now(),
+            title: newKanbanTitle,
+        }
+
+        setKanbans([...kanbans, kanban]);
+        setNewKanbanTitle('');
+        setIsCreateKanbanModalOpen(false);
+    }
+
     return (
-        <div className='max-w-[1280px] m-auto mt-[20px] flex gap-2'>
-            <div className='w-[480px] bg-amber-500 pt-[15px] pl-[10px] rounded'>
+        <div className='max-w-[1280px] m-auto mt-[20px] flex gap-2 overflow-x-auto'>
+            <div className='min-w-[270px] bg-amber-500 pt-[15px] pl-[10px] rounded'>
                 <span>К выполнению</span>
 
                 <div className='my-[10px] flex flex-col gap-2'>
@@ -37,18 +69,47 @@ export default function Dashboard(){
                         <div key={task.id} className='max-w-[100%] overflow-auto'>{task.title}</div>
                     )}
                     {isCreateTaskModalOpen ?
-                        <input type='text' onKeyDown={event => createTaskHandler(event)} placeholder='Название задачи' value={newTaskTitle} onChange={e => setNewTaskTitle(e.target.value)} className='bg-amber-500  text-white placeholder:text-white outline-none' /> :
+                        <input
+                            autoFocus
+                            type='text'
+                            value={newTaskTitle}
+                            onBlur={() => setIsCreateTaskModalOpen(false)}
+                            onKeyDown={event => createTaskHandler(event)}
+                            onChange={event => setNewTaskTitle(event.target.value)}
+                            className='bg-amber-500 outline-none placeholder:text-black'
+                        /> :
                         <button onClick={() => setIsCreateTaskModalOpen(true)} className='flex items-center gap-2'>
                             <FaPlus/>Создать
                         </button>
                     }
                 </div>
             </div>
-            <div className='w-[480px] bg-amber-500'>
+            <div className='min-w-[270px] bg-amber-500'>
                 <span>В работе</span>
             </div>
-            <div className='w-[480px] bg-amber-500'>
+            <div className='min-w-[270px] bg-amber-500'>
                 <div className='flex gap-2 items-center'>Готово <IoMdCheckmark /></div>
+            </div>
+
+            {kanbans.map(kanban =>
+                <div className='min-w-[270px] bg-amber-500'>
+                    <div className='flex gap-2 items-center'>{kanban.title}</div>
+                </div>
+            )}
+
+            <div className=''>
+                {isCreateKanbanModalOpen ?
+                    <input
+                        autoFocus
+                        type='text'
+                        value={newKanbanTitle}
+                        onBlur={() => setIsCreateKanbanModalOpen(false)}
+                        onKeyDown={(event) => createKanbanHandler(event)}
+                        onChange={event => setNewKanbanTitle(event.target.value)}
+                        className='bg-amber-500 outline-none placeholder:text-black'
+                    /> :
+                    <FaPlus onClick={() => setIsCreateKanbanModalOpen(true)} />
+                }
             </div>
         </div>
     )
