2723f757d8870c8cc032268ac266d51cac6b021f
Serhii Neliubov <sergeyneliubov@gmail.com>
2024-05-18 01:16:08 +0200

    style changes

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { IoMdCheckmark } from "react-icons/io";
 import axios from "axios";
 import { BsThreeDots } from "react-icons/bs";
+import {CiSearch} from "react-icons/ci";
 
 type Task = {
     id: number,
@@ -87,85 +88,99 @@ export default function Dashboard(){
     }
 
     return (
-        <div className='max-w-[1280px] m-auto mt-[20px] flex gap-2 overflow-x-auto'>
-            <div className='min-w-[270px] bg-amber-500 min-h-[600px] pt-[15px] pl-[10px] rounded'>
-                <span>К выполнению</span>
+        <div className='px-[40px] m-auto mt-[20px] flex gap-2 overflow-x-auto flex-col'>
+            <div className='max-w-[385px] mt-[30px] mb-[20px]'>
+                <h1 className='text-[24px] py-[5px] w-fit rounded font-semibold hover:bg-gray-200 transition-all'>
+                    Доска KANasd
+                </h1>
+            </div>
+            <div className='bg-white flex items-center max-w-[220px] rounded border border-gray-400 h-[40px] mb-[40px]'>
+                <CiSearch className='w-[25px] h-[25px] ml-[8px]'/>
+                <input type='text' className='w-[270px] h-[20px] bg-white rounded p-[5px] mr-[3px] outline-none' placeholder='Поиск'/>
+            </div>
+            <div className='flex gap-2'>
+                <div className='w-[270px] bg-amber-500 min-h-[600px] pt-[15px] pl-[10px] rounded'>
+                    <span>К выполнению</span>
 
-                <div className='my-[10px] flex flex-col gap-2'>
-                    {tasks.map(task =>
-                        <div key={task.id} className='max-w-[100%] overflow-auto'>{task.title}</div>
-                    )}
-                    {isCreateTaskModalOpen ?
-                        <input
-                            autoFocus
-                            type='text'
-                            value={newTaskTitle}
-                            onBlur={() => setIsCreateTaskModalOpen(false)}
-                            onKeyDown={event => createTaskHandler(event)}
-                            onChange={event => setNewTaskTitle(event.target.value)}
-                            className='bg-amber-500 outline-none placeholder:text-black'
-                        /> :
-                        <button onClick={() => setIsCreateTaskModalOpen(true)} className='flex items-center gap-2'>
-                            <FaPlus/>Создать
-                        </button>
-                    }
+                    <div className='my-[10px] flex flex-col gap-2'>
+                        {tasks.map(task =>
+                            <div key={task.id} className='max-w-[100%] overflow-auto'>{task.title}</div>
+                        )}
+                        {isCreateTaskModalOpen ?
+                            <input
+                                autoFocus
+                                type='text'
+                                value={newTaskTitle}
+                                onBlur={() => setIsCreateTaskModalOpen(false)}
+                                onKeyDown={event => createTaskHandler(event)}
+                                onChange={event => setNewTaskTitle(event.target.value)}
+                                className='bg-amber-500 outline-none placeholder:text-black'
+                            /> :
+                            <button onClick={() => setIsCreateTaskModalOpen(true)} className='flex items-center gap-2'>
+                                <FaPlus/>Создать
+                            </button>
+                        }
+                    </div>
                 </div>
-            </div>
-            <div className='min-w-[270px] bg-amber-500 min-h-[600px] pt-[15px] pl-[10px] rounded'>
-                <span>В работе</span>
-            </div>
-            <div className='min-w-[270px] bg-amber-500 min-h-[600px] pt-[15px] pl-[10px] rounded'>
-                <div className='flex gap-2 items-center'>Готово <IoMdCheckmark /></div>
-            </div>
+                <div className='w-[270px] bg-amber-500 min-h-[600px] pt-[15px] pl-[10px] rounded'>
+                    <span>В работе</span>
+                </div>
+                <div className='w-[270px] bg-amber-500 min-h-[600px] pt-[15px] pl-[10px] rounded'>
+                    <div className='flex gap-2 items-center'>Готово <IoMdCheckmark/></div>
+                </div>
 
-            {kanbans.map(kanban =>
-                <div
-                    onMouseEnter={() => setMouseEnteredKanban(kanban._id)}
-                    onMouseLeave={() => setMouseEnteredKanban('')}
-                    key={kanban._id}
-                    className='min-w-[270px] bg-[#f7f8f9] min-h-[600px] rounded'
-                >
+                {kanbans.map(kanban =>
                     <div
-                        onMouseEnter={() => setMouseEnteredKanbanTitle(kanban._id)}
-                        onMouseLeave={() => setMouseEnteredKanbanTitle('')}
-                        className='flex gap-2 py-[8px] px-[8px]'
+                        onMouseEnter={() => setMouseEnteredKanban(kanban._id)}
+                        onMouseLeave={() => setMouseEnteredKanban('')}
+                        key={kanban._id}
+                        className='min-w-[270px] bg-[#f7f8f9] min-h-[600px] rounded'
                     >
-                        <span className='w-full cursor-pointer h-[40px] pl-[8px] hover:bg-gray-300 transition-all rounded bg-gray-200'>{kanban.title}</span>
+                        <div
+                            onMouseEnter={() => setMouseEnteredKanbanTitle(kanban._id)}
+                            onMouseLeave={() => setMouseEnteredKanbanTitle('')}
+                            className='flex gap-2 py-[8px] px-[8px]'
+                        >
+                            <span
+                                className='w-full flex items-center cursor-pointer h-[40px] pl-[8px] hover:bg-gray-300 transition-all rounded bg-gray-200'>{kanban.title}</span>
 
-                        {mouseEnteredKanbanTitle === kanban._id && (
-                            <div onClick={() => deleteKanbanHandler(kanban._id)} className='bg-gray-200 p-[8px] rounded hover:cursor-pointer transition-all hover:bg-gray-300'>
-                                <BsThreeDots className='w-[24px] h-[24px]'/>
+                            {mouseEnteredKanbanTitle === kanban._id && (
+                                <div onClick={() => deleteKanbanHandler(kanban._id)}
+                                     className='bg-gray-200 p-[8px] rounded hover:cursor-pointer transition-all hover:bg-gray-300'>
+                                    <BsThreeDots className='w-[24px] h-[24px]'/>
+                                </div>
+                            )}
+                        </div>
+
+                        {mouseEnteredKanban === kanban._id && (
+                            <div
+                                className='flex gap-2 items-center mx-[8px] p-[8px] rounded hover:cursor-pointer transition-all hover:bg-gray-300'>
+                                <FaPlus className='w-[16px] h-[16px]'/>
+                                Create a task
                             </div>
                         )}
                     </div>
+                )}
 
-                    {mouseEnteredKanban === kanban._id && (
-                        <div className='flex gap-2 items-center mx-[8px] hover:bg-gray-200 p-[8px] rounded hover:cursor-pointer transition-all hover:bg-gray-300'>
-                            <FaPlus className='w-[16px] h-[16px]'/>
-                            Create a task
-                        </div>
-                    )}
-                </div>
-            )}
-
-            <div>
-                {isCreateKanbanModalOpen ?
-                    <div className='bg-gray-200 h-[42px] p-[5px] rounded'>
-                        <input
-                            autoFocus
-                            type='text'
-                            value={newKanbanTitle}
-                            onBlur={() => setIsCreateKanbanModalOpen(false)}
-                            onKeyDown={(event) => createKanbanHandler(event)}
-                            onChange={event => setNewKanbanTitle(event.target.value)}
-                            className='pl-[5px] rounded bg-none h-full placeholder:text-black outline-blue-500'
+                <div>
+                    {isCreateKanbanModalOpen ?
+                        <div className='bg-gray-200 h-[42px] p-[5px] rounded'>
+                            <input
+                                autoFocus
+                                type='text'
+                                value={newKanbanTitle}
+                                onBlur={() => setIsCreateKanbanModalOpen(false)}
+                                onKeyDown={(event) => createKanbanHandler(event)}
+                                onChange={event => setNewKanbanTitle(event.target.value)}
+                                className='pl-[5px] rounded bg-none h-full placeholder:text-black outline-blue-500'
+                            />
+                        </div> :
+                        <FaPlus
+                            onClick={() => setIsCreateKanbanModalOpen(true)}
+                            className='w-[42px] h-[42px] p-[13px] cursor-pointer hover:bg-gray-300 transition-all rounded bg-[#091e420f]'
                         />
-                    </div> :
-                    <FaPlus
-                        onClick={() => setIsCreateKanbanModalOpen(true)}
-                        className='w-[42px] h-[42px] p-[13px] cursor-pointer hover:bg-gray-300 transition-all rounded bg-[#091e420f]'
-                    />
-                }
+                    }
+                </div>
             </div>
         </div>
     )
