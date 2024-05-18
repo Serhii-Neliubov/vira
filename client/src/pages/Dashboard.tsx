1d5a1b476c9e2390407121cc65da5a52a698c04b
Serhii Neliubov <sergeyneliubov@gmail.com>
2024-05-18 15:32:19 +0200

    added dashboard title changing functional

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -21,6 +21,12 @@ type Kanban = {
 export default function Dashboard(){
     const API_URL = import.meta.env.VITE_API_URL;
 
+    // TODO: Remove static title when the title will be fetched from the server
+    const title = 'Доска KANasd';
+
+    const [dashboardTitle, setDashboardTitle] = useState(title);
+    const [dashboardTitleModalOpen, setDashboardTitleModalOpen] = useState(false);
+
     const [tasks, setTasks] = useState<Task[]>([]);
     const [kanbans, setKanbans] = useState<Kanban[]>([])
 
@@ -36,6 +42,14 @@ export default function Dashboard(){
     useEffect(() => {
         getKanbans().then(data => setKanbans(data));
     }, []);
+
+    function changeDashboardTitleHandler(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key !== 'Enter') {
+            return;
+        }
+
+        setDashboardTitleModalOpen(false);
+    }
 
     async function getKanbans() {
         try {
@@ -89,10 +103,22 @@ export default function Dashboard(){
 
     return (
         <div className='px-[40px] m-auto mt-[20px] flex gap-2 overflow-x-auto flex-col'>
-            <div className='max-w-[385px] mt-[30px] mb-[20px]'>
-                <h1 className='text-[24px] py-[5px] w-fit rounded font-semibold hover:bg-gray-200 transition-all'>
-                    Доска KANasd
-                </h1>
+            <div className='max-w-[400px] mt-[30px] mb-[20px]'>
+                {dashboardTitleModalOpen ?
+                    <input
+                        autoFocus
+                        maxLength={30}
+                        minLength={1}
+                        value={dashboardTitle}
+                        onChange={event => setDashboardTitle(event.target.value)}
+                        onKeyDown={event => changeDashboardTitleHandler(event)}
+                        onBlur={() => setDashboardTitleModalOpen(false)}
+                        className='h-[46px] text-[24px] outline-blue-500 pl-[5px] w-[420px] flex flex-nowrap'
+                    /> :
+                    <h1 onClick={() => setDashboardTitleModalOpen(true)} className='text-[24px] py-[5px] w-fit rounded font-semibold hover:bg-gray-200 transition-all'>
+                        {dashboardTitle}
+                    </h1>
+                }
             </div>
             <div className='bg-white flex items-center max-w-[220px] rounded border border-gray-400 h-[40px] mb-[40px]'>
                 <CiSearch className='w-[25px] h-[25px] ml-[8px]'/>
@@ -162,25 +188,24 @@ export default function Dashboard(){
                     </div>
                 )}
 
-                <div>
-                    {isCreateKanbanModalOpen ?
-                        <div className='bg-gray-200 h-[42px] p-[5px] rounded'>
+                {isCreateKanbanModalOpen ?
+                    <div className='bg-gray-200 h-[42px] p-[5px] rounded'>
                             <input
                                 autoFocus
                                 type='text'
+                                maxLength={30}
                                 value={newKanbanTitle}
                                 onBlur={() => setIsCreateKanbanModalOpen(false)}
                                 onKeyDown={(event) => createKanbanHandler(event)}
                                 onChange={event => setNewKanbanTitle(event.target.value)}
-                                className='pl-[5px] rounded bg-none h-full placeholder:text-black outline-blue-500'
+                                className='pl-[5px] rounded bg-none h-full w-[270px] placeholder:text-black outline-blue-500'
                             />
                         </div> :
-                        <FaPlus
-                            onClick={() => setIsCreateKanbanModalOpen(true)}
-                            className='w-[42px] h-[42px] p-[13px] cursor-pointer hover:bg-gray-300 transition-all rounded bg-[#091e420f]'
-                        />
-                    }
-                </div>
+                    <FaPlus
+                        onClick={() => setIsCreateKanbanModalOpen(true)}
+                        className='w-[42px] h-[42px] p-[13px] cursor-pointer hover:bg-gray-300 transition-all rounded bg-[#091e420f]'
+                    />
+                }
             </div>
         </div>
     )
